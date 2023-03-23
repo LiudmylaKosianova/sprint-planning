a22f08d417d21c897519ac5162acd2b5ab5f3ae5
Blueberry <snw.wht.lk@gmail.com>
2023-03-23 19:38:34 +0100

    UserStory

--- src/main/java/com/epam/rd/autotasks/sprintplanning/tickets/UserStory.java
+++ src/main/java/com/epam/rd/autotasks/sprintplanning/tickets/UserStory.java
@@ -1,23 +1,51 @@
 package com.epam.rd.autotasks.sprintplanning.tickets;
-
+/**
+ * UserStory
+ * We consider a user story to be a ticket that may contain some dependencies.
+ * A dependency is another user story that must be completed first to allow the dependent user story
+ * to complete. One provides dependencies of the UserStory via the constructor of the UserStory class.
+ * <p>
+ * complete() - Like the Ticket#complete() method, this sets the ticket to completed state.
+ * The difference is that the user story may not be completed if its dependencies are not completed yet.
+ * <p>
+ * getDependencies() - Returns a defensive copy of dependencies array.
+ * <p>
+ * toString() - Returns a String representing this user story, using its id and name.
+ * Example: "[US 1] User Registration Entity"
+ */
 public class UserStory extends Ticket {
 
-    public UserStory(int id, String name, int estimate, UserStory... dependsOn) {
+    UserStory [] dependencies;
+    public UserStory(int id, String name, int estimate, UserStory ...dependOn) {
         super(id, name, estimate);
-        throw new UnsupportedOperationException("Implement this method");
+        this.dependencies = dependOn;
+        //throw new UnsupportedOperationException("Implement this method");
     }
 
     @Override
     public void complete() {
-        throw new UnsupportedOperationException("Implement this method");
+
+        //throw new UnsupportedOperationException("Implement this method");
+        boolean foundNotCompleted = false;
+        for(UserStory x: dependencies){
+            if (!x.isCompleted()) {
+                foundNotCompleted = true;
+                break;
+            }
+        }
+        completed = !foundNotCompleted;
     }
 
     public UserStory[] getDependencies() {
-        throw new UnsupportedOperationException("Implement this method");
+        //throw new UnsupportedOperationException("Implement this method");
+        UserStory[] copy = new UserStory[dependencies.length];
+        System.arraycopy(dependencies, 0, copy, 0, dependencies.length);
+        return copy;
     }
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Implement this method");
+        return "[US " + this.getId() + "]" + " " + this.getName();
+        //throw new UnsupportedOperationException("Implement this method");
     }
 }
