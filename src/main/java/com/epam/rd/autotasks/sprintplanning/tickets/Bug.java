97abe3772dff63904afe0643b7385f44d03730f9
Blueberry <snw.wht.lk@gmail.com>
2023-03-23 19:48:02 +0100

    Bug

--- src/main/java/com/epam/rd/autotasks/sprintplanning/tickets/Bug.java
+++ src/main/java/com/epam/rd/autotasks/sprintplanning/tickets/Bug.java
@@ -1,18 +1,25 @@
 package com.epam.rd.autotasks.sprintplanning.tickets;
 
 public class Bug extends Ticket {
-
+UserStory userStory;
     public static Bug createBug(int id, String name, int estimate, UserStory userStory) {
-        throw new UnsupportedOperationException("Implement this method");
+        //throw new UnsupportedOperationException("Implement this method");
+        if(userStory==null || !userStory.isCompleted()){
+            return null;
+        }else{
+            return new Bug(id,name,estimate,userStory);
+        }
     }
 
     private Bug(int id, String name, int estimate, UserStory userStory) {
         super(id, name, estimate);
-        throw new UnsupportedOperationException("Implement this method");
+        //throw new UnsupportedOperationException("Implement this method");
+        this.userStory = userStory;
     }
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Implement this method");
+        //throw new UnsupportedOperationException("Implement this method");
+        return "[Bug " + this.getId() + "] " + this.userStory.getName()+": "+this.getName();
     }
 }
